838a637846e780a5b464e70e59eeecb1832aba9d
Chang Liu <cliu0174@student.monash.edu>
2025-07-12 16:17:52 +0800

    Lab3

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import Json from './components/JSON.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import Json from './components/JSON.vue'
+import LoginForm from './components/LoginForm.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <Json />
-  </main>
+  <!-- <Json /> -->
+  <LoginForm />
 </template>
 
 <style scoped>
